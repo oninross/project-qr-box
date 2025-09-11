213e443839ae2ad7533b812924226e2ba060dc67
Nino Rodriguez <oninross@gmail.com>
2025-09-11 13:27:34 +1000

    Remove unused image configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh5.googleusercontent.com',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
