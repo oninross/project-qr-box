78fe523813dd8c9d72ad5b1e7da2ae3ef8c3d4bc
Nino Rodriguez <oninross@gmail.com>
2025-09-16 15:44:47 +1000

    Enhance ESLint configuration by adding import plugin and custom rules; update package.json with clean-code scripts and ESLint version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,52 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import importPlugin from "eslint-plugin-import";
+import storybook from "eslint-plugin-storybook";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript"), {
-  ignores: [
-    "node_modules/**",
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ],
-}, ...storybook.configs["flat/recommended"]];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: { import: importPlugin },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin"],
+            ["external"],
+            ["internal"],
+            ["parent", "sibling", "index"],
+            ["object", "type"],
+            ["unknown"],
+          ],
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: { order: "asc", caseInsensitive: true },
+          "newlines-between": "always",
+          warnOnUnassignedImports: true,
+        },
+      ],
+      // Add other custom rules here
+    },
+  },
+  ...storybook.configs["flat/recommended"],
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+  },
+];
 
 export default eslintConfig;
