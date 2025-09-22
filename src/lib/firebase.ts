ff479a1cbdcb7164230e258617627630100fc305
Nino Rodriguez <oninross@gmail.com>
2025-09-22 12:41:54 +1000

    Add Firebase storage and analytics initialization to firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,8 @@
-// src/lib/firebase.ts
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -10,10 +11,17 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
+let analytics: ReturnType<typeof getAnalytics> | undefined = undefined;
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
 
-export { app, auth, db };
+export { app, auth, db, storage, analytics };
